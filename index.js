a6f8bd0d799c7ebfa7abf05404881f5af8f4786b
Shirish Kawatkar <kawatkar@esteco.com>
2021-05-08 22:36:08 +0530

    Added Search by pincode feature

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var app = express();
 var favicon = require('serve-favicon');
 var path = require('path');
 var moment = require('moment');
+var bodyParser = require('body-parser');
+var url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/"
+var today = new Date().toJSON().slice(0,10).split('-').reverse().join('-')
 
 const id = require('./id.json')
 
@@ -15,6 +18,7 @@ app.set("views", path.join(__dirname, 'public', 'views'));
 
 app.use('/',router);
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
@@ -32,20 +36,51 @@ const ten_days = () => {const days = []; const dateStart = moment(); const dateE
 
 // Route method 'get' 
 app.get('/', function (req, res){
-  var today = new Date().toJSON().slice(0,10).split('-').reverse().join('-')
   res.render("home", { title: "VaccineMON", district: id, today: today })
 })
 
 app.get('/:id/:date', function(req, res) {
-    var district_id = req.params.id
-    var date = req.params.date
-    var state = id.find(item => item.id === Number(req.params.id)).state
-    request.get({ url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" + district_id + "&date=" + date }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // console.log(ten_days())
-            res.render("info", { "district_id": district_id, "title": state, "results" : JSON.parse(body), "ten_days": ten_days()})
-            // res.json(body);
-        }
-    });
+  var district_id = req.params.id
+  var date = req.params.date
+  var state = id.find(item => item.id === Number(req.params.id)).state
+  request.get({ url: url + "calendarByDistrict?district_id=" + district_id + "&date=" + date }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(ten_days())
+      res.render("info", { "title": state, "results" : JSON.parse(body), "ten_days": ten_days()})
+      // res.json(body);
+    }
+    else {
+      res.render("error", {"title": pincode, "results" : JSON.parse(body)})
+    }
+  });
+});
+
+app.get('/pincode/:pincode/:date', function(req, res) {
+  var pincode = req.params.pincode
+  var date = req.params.date
+  request.get({ url: url + "calendarByPin?pincode=" + pincode + "&date=" + date }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(JSON.parse(body))
+      res.render("pincode", {"title": pincode, "results" : JSON.parse(body), "ten_days": ten_days()})
+      // res.json(body);
+    }
+    else {
+      res.render("error", {"title": pincode, "results" : JSON.parse(body)})
+    }
+  });
+});
+
+app.post('/', function(req, res) {
+  var pincode = req.body.pincode 
+  request.get({ url: url + "calendarByPin?pincode=" + pincode + "&date=" + today }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(JSON.parse(body))
+      res.render("pincode", {"title": pincode, "results" : JSON.parse(body), "ten_days": ten_days()})
+      // res.json(body);
+    }
+    else {
+      res.render("error", {"title": pincode, "results" : JSON.parse(body)})
+    }
+  });
 });
 
